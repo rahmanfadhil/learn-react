f267e6b51e604ef1d7ccfde50eefbe9ee71e0ba6
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-04-21 21:01:47 +0700

    Added react router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Layout from './components/Layout';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+// import Layout from './components/Layout';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Layout />, document.getElementById('root'));
+const Links = () => (
+  <nav>
+    <Link to="/">Home</Link><br/>
+    <Link to="/about">about</Link><br/>
+    <Link to="/contact">contact</Link><br/>
+  </nav>
+)
+
+ReactDOM.render(
+  <Router>
+    <div>
+      <Links />
+      <Route exact path="/" render={() => <h1>home</h1>}/>
+      <Route path="/about" render={() => <h1>about</h1>}/>
+      <Route path="/contact" render={() => <h1>contact</h1>}/>
+    </div>
+  </Router>
+  , document.getElementById('root')
+);
+
 registerServiceWorker();
